d47df8f25d9c0a460062f13ea8314224c26aa48c
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-07-10 03:33:21 +0530

    String divion problem solved

--- src/sorting/quick/QuickSort.java
+++ src/sorting/quick/QuickSort.java
@@ -1,4 +1,75 @@
 package sorting.quick;
 
 public class QuickSort {
+    public void quickSort(int[] arr, int lowerLimit, int upperLimit){
+        // Base Case
+        if(upperLimit <= lowerLimit){
+            return;
+        }
+      int pivotIndex = lowerLimit;
+      int larger = 0;
+      int smaller = 0;
+      // finding the right position of pivot
+        for (int i = lowerLimit + 1; i < upperLimit; i++) {
+            if(arr[pivotIndex] < arr[i]){
+                larger ++;
+            } else if (arr[pivotIndex] > arr[i]) {
+                smaller ++;
+            }
+        }
+        int correctIndex = lowerLimit + smaller;
+        System.out.println("larger : " + larger + " Smaller : " + smaller);
+        System.out.println("Right position will be at index " + smaller );
+        swapping(arr,pivotIndex, correctIndex);
+        pivotIndex = correctIndex;
+        // right position achieve
+
+        System.out.println("Pivot Index " + pivotIndex);
+
+        int firstPointer = lowerLimit;
+        int secondPointer = pivotIndex + 1;
+        while((firstPointer < pivotIndex) && (secondPointer <= upperLimit)) {
+            // first condition if firstPointer value is grater than pivot
+            if (arr[firstPointer] > arr[pivotIndex]) {
+                if (arr[pivotIndex] < arr[secondPointer]) {
+                    secondPointer++;
+                } else if (arr[pivotIndex] > arr[secondPointer]) {
+                    swapping(arr, firstPointer, secondPointer);
+                    firstPointer ++;
+                    secondPointer = pivotIndex + 1;
+                }
+            }
+            //second condition if firstPointer value is less than pivot
+            else{
+                firstPointer ++;
+            }
+        }
+
+        quickSort(arr, lowerLimit, pivotIndex -1);
+        quickSort(arr, pivotIndex + 1, upperLimit);
+
+    }
+    public void swapping(int [] arr, int pivot, int correctPosition){
+        int temp = arr[correctPosition];
+        arr[correctPosition] = arr[pivot];
+        arr[pivot] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5,6,2,4,7,9,1,3};
+        int lowerLimit = 0;
+        int upperLimit = arr.length - 1;
+        System.out.println("BEFORE SORTING");
+        for (int num: arr){
+            System.out.printf("%d,",num);
+        }
+        System.out.println();
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(arr, lowerLimit, upperLimit);
+        System.out.println("AFTER SORTING");
+        for(int num : arr){
+            System.out.print(num + ",");
+        }
+    }
 }
--- src/sorting/string/StringDivider.java
+++ src/sorting/string/StringDivider.java
@@ -1,4 +1,36 @@
 package sorting.string;
 
 public class StringDivider {
+    public StringBuilder[] stringDivider(String token){
+        StringBuilder header = new StringBuilder();
+        StringBuilder payload = new StringBuilder();
+        StringBuilder signature = new StringBuilder();
+
+        int dot = 0;
+
+        for (int i = 0; i < token.length(); i++) {
+            if (token.charAt(i) == '.') {
+                dot++;
+                continue; // dot ko kisi builder mein add nahi karna
+            }
+
+            if (dot == 0) {
+                header.append(token.charAt(i));
+            } else if (dot == 1) {
+                payload.append(token.charAt(i));
+            } else if (dot == 2) {
+                signature.append(token.charAt(i));
+            }
+        }
+        return new StringBuilder[]{header, payload, signature};
+    }
+
+    public static void main(String[] args) {
+        StringDivider stringDivider = new StringDivider();
+        StringBuilder [] returnedValue =
+                stringDivider.stringDivider("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5dWdhbmsifQ.xxxxx");
+        for(StringBuilder result : returnedValue){
+            System.out.println(result);
+        }
+    }
 }
