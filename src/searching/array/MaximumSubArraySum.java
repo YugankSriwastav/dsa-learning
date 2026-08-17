12b532bd982797b614a26378661d482f27b88243
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-08-17 16:12:34 +0530

    Better version of subarray sum

--- src/searching/array/MaximumSubArraySum.java
+++ src/searching/array/MaximumSubArraySum.java
@@ -24,5 +24,18 @@ public class MaximumSubArraySum {
         return maximum;
     }
     // Better
-
+    public static int maxSubArray(int []arr){
+        int size = arr.length;
+        int maximum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int first = 0; first < size; first ++) {
+            // inner loop
+            for(int second = first; second < size; second ++){
+                // in this approach we direct finding sum rather than printing subArray than adding them
+                sum += arr[second];
+                maximum= Math.max(sum,maximum);
+            }
+        }
+        return maximum;
+    }
 }
