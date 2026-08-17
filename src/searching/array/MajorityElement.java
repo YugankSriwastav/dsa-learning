6cef9e766ab0fee84e28d426ee86254869e86074
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-08-17 15:30:52 +0530

    solve the problem of majority element

--- src/searching/array/MajorityElement.java
+++ src/searching/array/MajorityElement.java
@@ -1,4 +1,33 @@
 package searching.array;
 
 public class MajorityElement {
+    public static void majorityElement(int [] arr){
+        int count = 0, candidate = 0, size = arr.length;
+        for (int num: arr) {
+            if(count == 0){
+                candidate = num;
+            }
+            if(num == candidate){
+                count ++;
+            }else{
+                count --;
+            }
+        }
+        count = 0;
+        for(int num : arr){
+            if(num == candidate){
+                count ++;
+            }
+        }
+        if(count > size / 2 ){
+            System.out.print("There " + "is " + candidate + " in majority");
+            return;
+        }
+        System.out.println("There is no element in majority");
+    }
+
+    public static void main(String[] args) {
+        int [] array = {2,2,1,1,1,2,2};
+        MajorityElement.majorityElement(array);
+    }
 }
