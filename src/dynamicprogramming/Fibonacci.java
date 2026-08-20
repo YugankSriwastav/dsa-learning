71594e6114f5b0b8692179b548f1a16ddf18f453
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-08-21 03:34:15 +0530

    solved fibonacci using better approach

--- src/dynamicprogramming/Fibonacci.java
+++ src/dynamicprogramming/Fibonacci.java
@@ -8,19 +8,35 @@ public class Fibonacci {
       if(x <= 1){
           return x;
       }
-      // agar valuew already malum hai to memo se le lo
       if(array[x] != -1){
           return array[x];
       }
-      array[x] = fibonacci(array,x -1) + fibonacci(array, x - 2);
-      return array[x];
+      // what if array doesn't contain the value of x
+        array[x] = fibonacci(array,x - 1) + fibonacci(array, x-2);
+        return array[x];
+    }
+
+    // Better Solution(Remove Recursion stack)
+    public long fibonacciBetter(long x, long[]array){
+        array[0] = 0;
+        array[1] = 1;
+        int n = array.length;
+        long value = 0;
+        for (int i = 2; i < n; i++) {
+
+            value = array[i - 1] + array[i-2];
+        }
+        return value;
     }
 
     public static void main(String[] args) {
+
         int x = 4;
-        long [] memo = new long[x + 1];
-        Arrays.fill(memo, -1);
-        System.out.println(fibonacci(memo, x));
+        long [] array = new long[x + 1];
+        Arrays.fill(array, -1);
+        System.out.println(fibonacci(array,x));
+        System.out.println("Better Approach");
+        System.out.println(fibonacci(array,x));
 
     }
 }
