37a0c8f006667aef8dc9e0f4eef6728bbd0e3018
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-08-22 13:02:42 +0530

    solve dp of frog jump

--- src/dynamicprogramming/FrogJump.java
+++ src/dynamicprogramming/FrogJump.java
@@ -1,24 +1,33 @@
 package dynamicprogramming;
 
+import java.util.Arrays;
+
 public class FrogJump {
 
-    public static long frogJump(int[]array, int start, int last){
+    public static long frogJump(int[]array, int start, int last, long[]dpMemory){
 
         // Base Case
         if(start == last){
             return 0;
         }
-        long firstValue =  Math.abs(array[start] - array[start + 1]) + frogJump(array, start + 1, last);
-        long secondValue = 0;
+        if(dpMemory[start] != -1){
+            return dpMemory[start];
+        }
+        long firstValue =  Math.abs(array[start] - array[start + 1]) + frogJump(array, start + 1, last,dpMemory);
+        long secondValue = Long.MAX_VALUE;
         if(start + 2 <= last){
-            secondValue = Math.abs(array[start] - array[start + 2]) + frogJump(array,start + 2, last);
+            secondValue = Math.abs(array[start] - array[start + 2]) + frogJump(array,start + 2, last,dpMemory);
         }
 
-        return Math.min(firstValue, secondValue);
+        dpMemory[start] = Math.min(firstValue, secondValue);
+        return dpMemory[start];
     }
 
     public static void main(String[] args) {
+
         int[] array = {10,20,30,10};
-        System.out.println(FrogJump.frogJump(array,0, array.length - 1));
+        long []frogMemory = new long[array.length];
+        Arrays.fill(frogMemory,-1);
+        System.out.println(FrogJump.frogJump(array,0, array.length - 1,frogMemory));
     }
 }
