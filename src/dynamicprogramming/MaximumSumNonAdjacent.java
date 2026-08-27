d13286d2e050de1ca18b75ade8fb10913c170152
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-08-27 13:14:23 +0530

    tabulation....

--- src/dynamicprogramming/MaximumSumNonAdjacent.java
+++ src/dynamicprogramming/MaximumSumNonAdjacent.java
@@ -1,5 +1,7 @@
 package dynamicprogramming;
 
+import java.util.Arrays;
+
 public class MaximumSumNonAdjacent {
     // Brute Force (Using Recursion)
     public long bruteForce(long[]arr, int start, int last) {
@@ -12,16 +14,33 @@ public class MaximumSumNonAdjacent {
         long pick = arr[start] + bruteForce(arr, start + 2, last);
         
         long noPick = bruteForce(arr, start + 1, last);
-        System.out.println(noPick);
         return maximum = Math.max(pick, noPick);
 
     }
 
+    public long memoization(long[] array, int first, int last, long[]dpMemory){
+        if(first >= last){
+            return 0;
+        }
+        if(dpMemory[first] != -1){
+            return dpMemory[first];
+        }
+        long pick = array[first] + memoization(array,first + 2, last, dpMemory);
+        long noPick = memoization(array,first + 1, last, dpMemory);
+        return dpMemory[first] = Math.max(pick, noPick);
+    }
+
+    // Most Imp Tabulation
+
     public static void main(String[] args) {
         long [] array = {2,7,9,3,1};
+        long []dpMemory = new long[array.length];
+        Arrays.fill(dpMemory, -1);
         MaximumSumNonAdjacent maxSum = new MaximumSumNonAdjacent();
         System.out.println("brute force");
         System.out.println( maxSum.bruteForce(array,0, array.length));
-
+        System.out.println("Memoization");
+        System.out.println(maxSum.memoization(array,0, array.length,dpMemory));
+        System.out.println("Tabulation");
     }
 }
