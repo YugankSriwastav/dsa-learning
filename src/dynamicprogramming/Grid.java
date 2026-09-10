9233a247e77b0f182bd935009c4ee646d7c85b80
Yugank Sriwastav <yuganksriwastav@gmail.com>
2026-09-11 02:14:42 +0530

    tabulation done

--- src/dynamicprogramming/Grid.java
+++ src/dynamicprogramming/Grid.java
@@ -48,6 +48,34 @@ public class Grid {
         return dpMemory[var1][var2] = right + down;
     }
 
+    // Tabulation
+
+    public long tabulation(long [][] array, int var1, int var2, int m, int n){
+
+        long [][]dpMemory = new long[3][3];
+        for (int i = 0; i < 3; i++) {
+            Arrays.fill(dpMemory[i], -1);
+        }
+
+        // Base Case
+        if(var1 > m -1 || var2 > n - 1){
+            return 0;
+        }
+
+        dpMemory[0][0] = 1;
+        dpMemory[0][1] = 1;
+        dpMemory[0][2] = 1;
+        dpMemory[1][0] = 1;
+        dpMemory[2][0] = 1;
+
+        for (int i = 1; i < 3; i++) {
+            for (int j = 1; j < 3; j++) {
+                dpMemory[i][j] = dpMemory[i - 1][j] + dpMemory[i][j - 1];
+            }
+        }
+        return dpMemory[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         long[][] arr = {
                 {10, 20, 30},
@@ -61,10 +89,12 @@ public class Grid {
 
         Grid grid = new Grid();
         long result =  grid.recursion(arr,0,0, arr.length, arr[0].length);
-        System.out.println(STR."Your path is : \{result}");
+        System.out.println(STR."Your path is using recursion : \{result}");
 
         System.out.println("Memoization");
         System.out.println(grid.memoization(arr,dpMemory,0,0, arr.length, arr[0].length));
+        System.out.println("Tabulation");
+        System.out.println(grid.tabulation(arr,0,0, arr.length, arr[0].length));
 
     }
 
